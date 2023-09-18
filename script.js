d98f5584d0153ea12c93729ce9f7bda4c647b414
amh512 <141826682+amh512@users.noreply.github.com>
2023-09-18 15:29:21 +0330

    js ورود

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const form = document.getElementById("loginForm");
-  const phoneInput = document.getElementById("phone");
-  const usernameInput = document.getElementById("username");
-  const submitButton = document.querySelector('input[type="submit"]');
-
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    form.reportValidity();
+    const form = document.getElementById("loginForm");
+    const phoneInput = document.getElementById("phone");
+    const usernameInput = document.getElementById("username");
+    const submitButton = document.querySelector('input[type="submit"]');
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      form.reportValidity();
+    });
+  
+    submitButton.addEventListener("click", function() {
+      form.reportValidity();
+    });
+  
+    phoneInput.addEventListener("invalid", function() {
+      const errorMessage = document.querySelector('label[for="phone"].error-message');
+      errorMessage.innerText = ".شماره موبایل نامعتبر میباشد";
+    });
+  
+    phoneInput.addEventListener("input", function() {
+      const errorMessage = document.querySelector('label[for="phone"].error-message');
+      errorMessage.innerText = "";
+    });
+  
+    usernameInput.addEventListener("invalid", function() {
+      const errorMessage = document.querySelector('label[for="username"].error-message');
+      errorMessage.innerText = ".لطفاً یک نام کاربری انتخاب کنید";
+    });
+  
+    usernameInput.addEventListener("input", function() {
+      const errorMessage = document.querySelector('label[for="username"].error-message');
+      errorMessage.innerText = "";
+    });
   });
-
-  submitButton.addEventListener("click", function() {
-    form.reportValidity();
-  });
-
-  phoneInput.addEventListener("invalid", function() {
-    const errorMessage = document.querySelector('label[for="phone"].error-message');
-    errorMessage.innerText = ".شماره موبایل نامعتبر میباشد";
-  });
-
-  phoneInput.addEventListener("input", function() {
-    const errorMessage = document.querySelector('label[for="phone"].error-message');
-    errorMessage.innerText = "";
-  });
-
-  usernameInput.addEventListener("invalid", function() {
-    const errorMessage = document.querySelector('label[for="username"].error-message');
-    errorMessage.innerText = ".لطفاً یک نام کاربری انتخاب کنید";
-  });
-
-  usernameInput.addEventListener("input", function() {
-    const errorMessage = document.querySelector('label[for="username"].error-message');
-    errorMessage.innerText = "";
-  });
-});
